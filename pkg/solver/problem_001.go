c7b4cc3f671bfc2b89ba95068aa4351425ed9173
Takao Shibata <chise.alter.pasta@gmail.com>
2021-02-24 00:00:55 +0900

    Solve problem 001

--- pkg/solver/problem_001.go
+++ pkg/solver/problem_001.go
@@ -5,5 +5,11 @@ import "math/big"
 type problem001Solver struct{}
 
 func (s *problem001Solver) Solve() (*big.Int, error) {
-	return big.NewInt(233168), nil
+	var sum int64 = 0
+	for i := 1; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += int64(i)
+		}
+	}
+	return big.NewInt(sum), nil
 }
